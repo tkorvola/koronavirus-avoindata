e25397beb2ce39ee2cd37d4909034ef454719410
Timo Korvola <tkorvola@iki.fi>
2020-04-26 21:51:07 +0300

    Also study just more recent data.

--- test.r
+++ test.r
@@ -8,19 +8,24 @@ if (is.unsorted(conf.agg$date)) conf.agg <- conf.agg[order(conf.agg$date),]
 conf.agg <- transform(conf.agg, n=cumsum(dn),
                       days=as.double(difftime(date, date[1], units="d")))
 
-tests <- (fromJSON("testdata.json")$tested
-    %>% transform(date=as.POSIXct(date)) %>% rename(tested=value))
+(fromJSON("testdata.json")$tested
+    %>% transform(date=as.POSIXct(date)) %>% rename(tested=value)
+    -> tests)
 
 tc <- merge(tests, conf.agg, by="date", all=TRUE)
 tc$dn[is.na(tc$dn)] <- 0
 tc <- filter(tc, dn != 0 | tested != 0)
-tc.sane <- filter(tc, dn <= tested)
-tc.lm <- lm(dn ~ tested + n, tc.sane)
+d.new <- "2020-03-28"
+tc %>% filter(date >= d.new) -> tc.new
+tc.lm <- lm(dn ~ tested + n, tc)
+tc.lm2 <- lm(dn ~ tested + n, tc.new)
 
 pdf("test.pdf", title="Testing")
-plot(tc.sane[, c("days", "tested", "n", "dn")])
+plot(tc[, c("days", "tested", "n", "dn")])
 ## print(qplot(date, tested, data=tc) + geom_point(aes(y=dn), colour="red")
 ##       + scale_y_log10())
+plot(tc.new[, c("days", "tested", "n", "dn")], main=paste("Alkaen", d.new))
 dev.off()
 
 summary(tc.lm)
+summary(tc.lm2)
