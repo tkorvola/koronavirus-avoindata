1325ad8bf01e487c08c6cfd2d71df053b1dc8070
Timo Korvola <tkorvola@iki.fi>
2020-04-26 22:11:13 +0300

    Plot tested vs. time on log scale.

--- test.r
+++ test.r
@@ -21,7 +21,12 @@ tc.lm <- lm(dn ~ tested + n, tc)
 tc.lm2 <- lm(dn ~ tested + n, tc.new)
 
 pdf("test.pdf", title="Testing")
-plot(tc[, c("days", "tested", "n", "dn")])
+print(qplot(date, tested, data=tc[tc$tested > 0,])
+      + scale_y_log10() + annotation_logticks(sides="l")
+      + geom_vline(xintercept=as.POSIXct(d.new), linetype="dotted")
+      + ggtitle(paste("Raja", d.new)))
+plot(tc[, c("days", "tested", "n", "dn")],
+     main=paste("Kaikki:", min(tc$date), "-", max(tc$date)))
 ## print(qplot(date, tested, data=tc) + geom_point(aes(y=dn), colour="red")
 ##       + scale_y_log10())
 plot(tc.new[, c("days", "tested", "n", "dn")], main=paste("Alkaen", d.new))
